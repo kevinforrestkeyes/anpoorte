766e2499425b9df13ab9fb96bc49e8fe604e7c8a
kevinforrestkeyes <kevinforrestkeyes@gmail.com>
2019-09-29 20:35:29 -0700

    hooking up to app successfully

--- index.js
+++ index.js
@@ -1,27 +1,123 @@
-import { getAllProducts, addNewProduct } from './lib/shopify';
-
+const dotenv = require('dotenv').config();
 const express = require('express');
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce')();
+const querystring = require('querystring');
+const request = require('request-promise');
 const app = express();
+const Shopify = require('shopify-api-node');
 
-app.get('/', function (req, res) {
-  res.send('anpoorte birb')
+const apiKey = process.env.SHOPIFY_API_KEY;
+const apiSecret = process.env.SHOPIFY_API_SECRET;
+const scopes = 'read_products,write_products';
+const forwardingAddress = "https://a45df053.ngrok.io";
+let accessToken;
+let shopName;
+
+app.get('/', (req, res) => {
+  res.send({ respone: 'anpoort birb' });
 });
 
-app.get('/get-shopify-products', function (req, res) {
-	const logIndex = newLog('get-shopify-products');
-	getAllProducts()
-		.then((products) => res.send(products))
-		.catch((err) => console.error(err));
-	endLog(logIndex);
+app.get('/get-auth-status', (req, res) => {
+	const authStatus = accessToken !== undefined;
+	res.send({ authorized: authStatus });
+})
+
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+		const redirectUri = forwardingAddress + '/auth/callback';
+    const installUrl = 'https://' + shop +
+      '/admin/oauth/authorize?client_id=' + apiKey +
+      '&scope=' + scopes +
+      '&state=' + state +
+      '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
 });
 
-app.get('/add-shopify-product', function (req, res) {
-	addNewProduct()
-		.then((product) => res.send(product))
-		.catch((err) => console.error(err));
+app.get('/auth/callback', (req, res) => {
+	const { shop, hmac, code, state } = req.query;
+	shopName = shop;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    const map = Object.assign({}, req.query);
+		delete map['signature'];
+		delete map['hmac'];
+		const message = querystring.stringify(map);
+		const providedHmac = Buffer.from(hmac, 'utf-8');
+		const generatedHash = Buffer.from(
+			crypto
+				.createHmac('sha256', apiSecret)
+				.update(message)
+				.digest('hex'),
+				'utf-8'
+			);
+		let hashEquals = false;
+		try {
+			hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+		} catch (e) {
+			hashEquals = false;
+		};
+
+		if (!hashEquals) {
+			return res.status(400).send('HMAC validation failed');
+		}
+
+		const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+		const accessTokenPayload = {
+			client_id: apiKey,
+			client_secret: apiSecret,
+			code,
+		};
+
+		request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+		.then((accessTokenResponse) => {
+			accessToken = accessTokenResponse.access_token;
+			res.redirect('http://localhost:8081/')
+		})
+		.catch((error) => {
+			res.status(error.statusCode).send(error.error.error_description);
+		});
+  } else {
+    res.status(400).send('Required parameters missing');
+  }
 });
 
-const PORT = process.env.PORT || 2222;
+app.get('/get-products', (req, res) => {
+	const shopify = new Shopify({
+		shopName,
+		accessToken,
+	});
+
+	shopify
+		.product
+		.create({
+			"title": "crappy pet store nuggets",
+			"body_html": "<strong>CRAPS!</strong>",
+			"vendor": "jimym",
+			"product_type": "Snowboard",
+			"tags": "Barnes & Noble, John's Fav, \"Big Air\""
+		})
+		.then(
+			(product) => res.send(product), 
+			(err) => console.log(err)
+		);
+	
+})
+
+const PORT = process.env.PORT || 4444;
 app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`);
+  console.log('birb on 4444');
 });
